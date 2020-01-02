29797703dec2faa870549f2ecd346e22d8dbb22f
Hunter Pugh <34586837+ZanyHunter@users.noreply.github.com>
2020-01-01 21:11:32 -0500

    Optimize for versatility

--- src/main/java/frc/robot/commands/DriveTeleop.java
+++ src/main/java/frc/robot/commands/DriveTeleop.java
@@ -20,18 +20,16 @@ public class DriveTeleop extends CommandBase {
     addRequirements(m_swerveDrive);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    //Change to match joystick configuration
     RobotContainer.swerveDrive.drive(RobotContainer.xbox.getRawAxis(0), RobotContainer.xbox.getRawAxis(4), RobotContainer.xbox.getRawAxis(4));
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
   }
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;

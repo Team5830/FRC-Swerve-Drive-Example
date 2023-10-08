f436ccb643a5840cd71ede2f9f648bcb0fd993d2
Aaron Steele <asteele11@yahoo.com>
2023-10-07 21:49:13 -0400

    add robotcontainer and settings

--- src/main/java/frc/robot/commands/DriveTeleop.java
+++ src/main/java/frc/robot/commands/DriveTeleop.java
@@ -1,19 +1,25 @@
 package frc.robot.commands;
 
-import frc.robot.OI;
-import frc.robot.Robot;
+import frc.robot.subsystems.*;
+import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 /**
  *
  */
 public class DriveTeleop extends CommandBase {
-
-    public DriveTeleop() {}
+    private SwerveDrive swervedrive;
+    private DoubleSupplier x;
+    private DoubleSupplier y;
+    public DriveTeleop(DoubleSupplier X, DoubleSupplier Y, SwerveDrive swerveDrive) {
+        x = X;
+        y = Y;
+        swervedrive = swerveDrive;
+    }
     @Override
     public void execute() {
     	//In this specific setup, Xbox left stick moves and strafes, right stick rotates
-    	Robot.swerveDrive.drive(OI.xbox.getRawAxis(0), OI.xbox.getRawAxis(4), OI.xbox.getRawAxis(4));
+    	swervedrive.drive(x.getAsDouble(), y.getAsDouble(), y.getAsDouble());
     }
     @Override
     public boolean isFinished() {
